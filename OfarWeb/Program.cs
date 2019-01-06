4a8052b247064a831ecf2f54a76597512e24e3df
Ari Johannesson <arividar@gmail.com>
2019-01-06 14:35:34 +0000

    Minor refactoring

--- OfarWeb/Program.cs
+++ OfarWeb/Program.cs
@@ -4,9 +4,6 @@ namespace OfarWeb
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hey there OfarWeb!");
-        }
+        static void Main(string[] args) => Console.WriteLine("Hey there OfarWeb!");
     }
 }
