e4ffc3b200e35751ab16266694cc21eccb975c76
Ari Johannesson <arividar@gmail.com>
2019-01-12 23:34:01 +0000

    Builds WebHost from scratch, just for fun

--- OfarWeb/Program.cs
+++ OfarWeb/Program.cs
@@ -1,15 +1,107 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HostFiltering;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace OfarWeb
 {
     public class Program
     {
+        internal class HostFilteringStartupFilter: IStartupFilter
+        {
+            public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+            {
+                return app =>
+                {
+                    app.UseHostFiltering();
+                    next(app);
+                };
+            }
+        }   
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Hey there OfarWeb!");
-            IWebHost wh = WebHost.CreateDefaultBuilder<Startup>(args).Build();
+            var builder = new WebHostBuilder();
+
+            if (string.IsNullOrEmpty(builder.GetSetting(WebHostDefaults.ContentRootKey)))
+            {
+                builder.UseContentRoot(Directory.GetCurrentDirectory());
+            }
+            if (args != null)
+            {
+                builder.UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build());
+            }
+
+            builder.UseKestrel((builderContext, options) =>
+            {
+                options.Configure(builderContext.Configuration.GetSection("Kestrel"));
+            })
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+                    if (env.IsDevelopment())
+                    {
+                        var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+                        if (appAssembly != null)
+                        {
+                            config.AddUserSecrets(appAssembly, optional: true);
+                        }
+                    }
+
+                    config.AddEnvironmentVariables();
+
+                    if (args != null)
+                    {
+                        config.AddCommandLine(args);
+                    }
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
+                })
+                .ConfigureServices((hostingContext, services) =>
+                {
+                    // Fallback
+                    services.PostConfigure<HostFilteringOptions>(options =>
+                    {
+                        if (options.AllowedHosts == null || options.AllowedHosts.Count == 0)
+                        {
+                            // "AllowedHosts": "localhost;127.0.0.1;[::1]"
+                            var hosts = hostingContext.Configuration["AllowedHosts"]?.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                            // Fall back to "*" to disable.
+                            options.AllowedHosts = (hosts?.Length > 0 ? hosts : new[] { "*" });
+                        }
+                    });
+                    // Change notification
+                    services.AddSingleton<IOptionsChangeTokenSource<HostFilteringOptions>>(
+                        new ConfigurationChangeTokenSource<HostFilteringOptions>(hostingContext.Configuration));
+
+                    services.AddTransient<IStartupFilter, HostFilteringStartupFilter>();
+                })
+                .UseIIS()
+                .UseIISIntegration()
+                .UseDefaultServiceProvider((context, options) =>
+                {
+                    options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+                });
+
+            builder.UseStartup<Startup>();
+            IWebHost wh = builder.Build();
             wh.Run();
         }
     }
