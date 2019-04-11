bd624e14a2c5f868600a6cf39ef6d34e589ebb73
Ari Johannesson <arividar@gmail.com>
2019-04-11 17:26:28 +0000

    Removes unnecessary using

--- OfarWeb/Program.cs
+++ OfarWeb/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HostFiltering;
 using Microsoft.AspNetCore.Hosting;
